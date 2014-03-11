d01a8e704e76c2a5c30caaee5c5e65115e4c773e
Hard Light <hardlightxda@gmail.com>
2014-03-11 15:28:08 +0200

    Another try to fix TWRP brightness
    TWRP cannot read linked  brightness file, so providing real location shold help.
    Line from recovery.log:
    I:Cannot find file "/sys/class/leds/lcd-backlight/brightness"
    
    Change-Id: If3a0aecc7558a2aae2df4f42974319483f72ed1b

--- BoardConfigRecovery.mk
+++ BoardConfigRecovery.mk
@@ -5,7 +5,7 @@ TARGET_NO_INITLOGO := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
-TW_BRIGHTNESS_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+TW_BRIGHTNESS_PATH := \"/sys/devices/platform/i2c-gpio.2/i2c-2/2-0060/leds/lcd-backlight/brightness\"
 TW_EXCLUDE_SUPERSU := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 TW_MAX_BRIGHTNESS := 255
